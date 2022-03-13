6c515278d6b59bdd9144dd9b61c1df2c28442ee9
Bhavanimattaparthi219 <84083696+Bhavanimattaparthi219@users.noreply.github.com>
2022-03-13 14:55:51 +0530

    Add files via upload

---
+++ hackerrank c/hello world.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char arr[20];
+    gets(arr);
+    printf("Hello, World!\n");
+    puts(arr);
+    return 0;
+}
